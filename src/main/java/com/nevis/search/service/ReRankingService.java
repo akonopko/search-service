96c41545c2c86f66024bbc44272e67dbf68d5c58
Alexander Konopko <alexander.konopko@gmail.com>
2026-01-05 11:35:55 +0000

    refactor: remove unused interface

--- src/main/java/com/nevis/search/service/ReRankingService.java
+++
@@ -1,9 +0,0 @@
-package com.nevis.search.service;
-
-import java.util.List;
-
-public interface ReRankingService {
-
-    List<DocumentSearchResult> reRank(String query, List<DocumentSearchResult> candidates);
-
-}
